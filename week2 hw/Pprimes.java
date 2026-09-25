e3e353a3c01a260aa103c983b2bd043fb3c756db
ElvinGarabayli <egarabayli23202@ada.edu.az>
2026-09-25 15:56:00 +0400

    file saved in wrong format so i corrected it

--- week2 hw/Pprimes.java
+++ week2 hw/Pprimes.java
@@ -1,46 +1,41 @@
 public class Pprimes {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
-        if(a%2==0){
+        if (a % 2 == 0) {
             System.out.println("Not prime");
         }
-        if(a%3==0){
+
+        if (a % 3 == 0) {
             System.out.println("Not prime");
         }
-        if(a%4==0){
+
+        if (a % 4 == 0) {
             System.out.println("Not prime");
         }
-        if(a%5==0){
+
+        if (a % 5 == 0) {
             System.out.println("Not prime");
         }
-        if(a%6==0){
+
+        if (a % 6 == 0) {
             System.out.println("Not prime");
         }
-        if(a%7==0){
+
+        if (a % 7 == 0) {
             System.out.println("Not prime");
         }
-        if(a%8==0){
+
+        if (a % 8 == 0) {
             System.out.println("Not prime");
         }
-        if(a%9==0){
+
+        if (a % 9 == 0) {
             System.out.println("Not prime");
         }
-        if(a%2!=0){
-            if(a%3!=0){
-                if(a%4!=0){
-                    if(a%5!=0){
-                        if(a%6!=0){
-                            if(a%7!=0){
-                                if(a%8!=0){
-                                    if(a%9!=0){
-                                        System.out.println("Prime");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+
+        if (a % 2 != 0 && a % 3 != 0 && a % 4 != 0 && a % 5 != 0 && a % 6 != 0 && a % 7 != 0 && a % 8 != 0 && a % 9 != 0) {
+            System.out.println("Prime");
         }
+
     }
 }
