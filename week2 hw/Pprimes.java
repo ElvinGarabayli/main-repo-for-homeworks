61e6b57f7252020f3aa1153295315509842e9964
ElvinGarabayli <egarabayli23202@ada.edu.az>
2026-09-25 15:50:28 +0400

    Add files via upload

---
+++ week2 hw/Pprimes.java
@@ -0,0 +1,46 @@
+public class Pprimes {
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        if(a%2==0){
+            System.out.println("Not prime");
+        }
+        if(a%3==0){
+            System.out.println("Not prime");
+        }
+        if(a%4==0){
+            System.out.println("Not prime");
+        }
+        if(a%5==0){
+            System.out.println("Not prime");
+        }
+        if(a%6==0){
+            System.out.println("Not prime");
+        }
+        if(a%7==0){
+            System.out.println("Not prime");
+        }
+        if(a%8==0){
+            System.out.println("Not prime");
+        }
+        if(a%9==0){
+            System.out.println("Not prime");
+        }
+        if(a%2!=0){
+            if(a%3!=0){
+                if(a%4!=0){
+                    if(a%5!=0){
+                        if(a%6!=0){
+                            if(a%7!=0){
+                                if(a%8!=0){
+                                    if(a%9!=0){
+                                        System.out.println("Prime");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
